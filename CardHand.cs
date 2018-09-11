2659c9d5d07dc12f53ae867cf0ebf64742385eea
dstovell <ninja@Daniels-iMac.local>
2018-09-11 15:22:55 -0700

    Card stuff

---
+++ CardHand.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VoxelSpace
+{
+	public class CardHand : MonoBehaviour
+	{
+		public UltimateJoystick.Anchor anchor = UltimateJoystick.Anchor.Right;
+
+		public CardWidget CardWidgetPrefab;
+
+		public const int MaxCards = 5;
+		public const float CardHeight = 18;
+		public const float CardWidth = 10;
+
+		public CardWidget [] Widgets;
+		public List<Card> Cards = new List<Card>();
+
+		void Awake()
+		{
+			this.Cards = new List<Card>();
+			this.Widgets = new CardWidget[CardHand.MaxCards];
+
+			for (int i=0; i<this.Widgets.Length; i++)
+			{
+				this.Widgets[i] = GameObject.Instantiate(this.CardWidgetPrefab, this.transform);
+			}
+
+			this.UpdateCardPositions();
+		}
+
+		public void AddCard()
+		{
+			
+		}
+
+		public void UpdateCardPositions()
+		{
+			int requiredCards = 1;
+			float fullHeight = 100f;
+			float requiredHeight = (float)requiredCards * CardHand.CardHeight;
+			float startHeight = (fullHeight - requiredHeight)/2f + CardHand.CardHeight/2f;
+			//Debug.LogError("requiredHeight: " + requiredHeight + " fullHeight: " + fullHeight + " startHeight: " + startHeight);
+
+			float xRowOne = 4;
+			float xRowTwo = xRowOne + CardHand.CardWidth/2;
+
+			float x = xRowOne;
+			float y = startHeight;
+			for (int i=0; i<this.Widgets.Length; i++)
+			{
+				CardWidget cw = this.Widgets[i];
+				if (i < requiredCards)
+				{
+					//cw.SetCard(this.Cards[i]);
+					cw.SetCard(null);
+					cw.SetPosition(anchor, x, y);
+
+					x = (x == xRowOne) ? xRowTwo : xRowOne;
+					y += CardHand.CardHeight;
+				}
+				else 
+				{
+					cw.ClearCard();
+				}
+			}
+		}
+	}
+}
+
---
+++ CardWidget.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VoxelSpace
+{
+	public class CardWidget : MonoBehaviour
+	{
+		public UltimateJoystick Draggable;
+		public Card ActiveCard;
+
+		public bool WasDragging = false;
+		public Vector3 LastDragPosition;
+
+		public void SetPosition(UltimateJoystick.Anchor anchor, float x, float y)
+		{
+			if (this.Draggable != null)
+			{
+				this.Draggable.anchor = anchor;
+				this.Draggable.customSpacing_X = x;
+				this.Draggable.customSpacing_Y = y;
+				this.Draggable.UpdatePositioning();
+			}
+		}
+
+		public void SetCard(Card c)
+		{
+			this.ActiveCard = c;
+
+			this.Show();
+		}
+
+		public void ClearCard()
+		{
+			this.ActiveCard = null;
+
+			this.Hide();
+		}
+
+		private void Hide()
+		{
+			this.gameObject.SetActive(false);
+		}
+
+		private void Show()
+		{
+			this.gameObject.SetActive(true);
+		}
+
+		void SpawnAt(Vector3 pos)
+		{
+			Debug.LogError("SpawnAt!!! pos:" + pos);
+		}
+
+		void Update()
+		{
+			if (this.Draggable != null)
+			{
+				bool isDragging = (this.Draggable.GetDistance() > 0f);
+
+				if (!this.WasDragging && isDragging)
+				{
+					//Debug.LogError("DragStarted!!! customSpacing_X:" + this.Draggable.customSpacing_X + " customSpacing_Y:" + this.Draggable.customSpacing_Y);
+				}
+				else if (this.WasDragging && !isDragging)
+				{
+					this.SpawnAt(this.LastDragPosition);
+				}
+				this.WasDragging = isDragging;
+				this.LastDragPosition = this.Draggable.joystick.position;
+			}
+		}
+	}
+}
+
